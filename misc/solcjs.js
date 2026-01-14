548a39a840ad5f30ec4fc04da62b90ff8780cc2e
Connector-Tool <146320523+Connector-Tool@users.noreply.github.com>
2026-01-14 14:45:11 +0800

    Delete misc directory

--- misc/solcjs.js
+++
@@ -1,7 +0,0 @@
-const solc = require('solc%s');
-const input = %s;
-var output = solc.compile(JSON.stringify(input));
-if (output instanceof Object) {
-  output = JSON.stringify(output);
-}
-console.log(output)
